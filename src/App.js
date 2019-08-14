a4e291d693e7bb9c6074bdc7b6b2af3d127e08d0
bnagen <dxrock1@gmail.com>
2019-08-14 13:45:31 -0700

    starting working on "more" button giving information on the user

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, {Fragment} from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+
 import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
@@ -33,6 +36,15 @@ class App extends React.Component {
     this.setState({users: res.data.items, loading: false});
   }
 
+  // Get single user
+  getUser = async (username) => {
+    this.setState({loading: true});
+
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({user: res.data, loading: false});
+  }
+
   clearUsers = () => {
     this.setState({users: [], loading: false});
   }
@@ -43,7 +55,7 @@ class App extends React.Component {
     setTimeout(() => this.setState({alert: null}), 3000);
   }
   render() {
-    const {users, loading } = this.state;
+    const {users, user, loading } = this.state;
     return (
       <Router>
       <div className='App'>
@@ -58,6 +70,10 @@ class App extends React.Component {
               </Fragment>
             )}></Route>
             <Route exact path='/about' component={About}></Route>
+            <Route exact path='/user/:login' render={props => (
+              <User { ...props} getUser={this.getUser} user={user} loading={loading}></User>
+              
+            )}></Route>
           </Switch>
           
         </div>
